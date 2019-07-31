bb28184717e3974a8e37ffc96e4c81ff8370f4f7
minhan <alsgks1218@gmail.com>
2019-07-31 20:26:26 +0900

    first server and print hello world

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Listening on: http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
+
+app.get("/", function(req, res) {
+  res.send("Hello World");
+});
