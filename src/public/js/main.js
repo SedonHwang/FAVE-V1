538bc77cce28efd71d7e83c9f94b1ba75b5f45b7
unknown <alsgks1218@naver.com>
2019-08-26 21:57:46 +0900

    side navi item and collapse

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,6 +2,7 @@ const openSide = document.getElementById("js--openSide");
 const closeSide = document.getElementById("js--closeSide");
 const logoResponsive = document.getElementById("js--logoResponsive");
 const sideNavi = document.getElementById("js--sideNavi");
+const collasible = document.getElementById("js--collapsible");
 
 openSide.addEventListener("click", function() {
   logoResponsive.classList.add("hide");
@@ -11,4 +12,21 @@ openSide.addEventListener("click", function() {
 closeSide.addEventListener("click", function() {
   logoResponsive.classList.remove("hide");
   sideNavi.classList.remove("visible");
+});
+
+collasible.addEventListener("click", function() {
+  const ftCaret = this.children[1].children[0].children[0];
+  const naviContent = this.nextElementSibling;
+
+  if (!naviContent.classList.contains("visible")) {
+    this.classList.add("active");
+    naviContent.classList.add("visible");
+    ftCaret.classList.remove("fa-caret-down");
+    ftCaret.classList.add("fa-caret-up");
+  } else {
+    this.classList.remove("active");
+    naviContent.classList.remove("visible");
+    ftCaret.classList.remove("fa-caret-up");
+    ftCaret.classList.add("fa-caret-down");
+  }
 });
