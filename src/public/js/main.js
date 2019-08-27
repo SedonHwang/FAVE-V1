e2932bc995d41834deaeb5873065fe25910ed6d4
unknown <alsgks1218@naver.com>
2019-08-27 10:20:33 +0900

    side navi version 1 complete

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,7 +2,7 @@ const openSide = document.getElementById("js--openSide");
 const closeSide = document.getElementById("js--closeSide");
 const logoResponsive = document.getElementById("js--logoResponsive");
 const sideNavi = document.getElementById("js--sideNavi");
-const collasible = document.getElementById("js--collapsible");
+const collasible = document.getElementsByClassName("js--collapsible");
 
 openSide.addEventListener("click", function() {
   logoResponsive.classList.add("hide");
@@ -14,19 +14,21 @@ closeSide.addEventListener("click", function() {
   sideNavi.classList.remove("visible");
 });
 
-collasible.addEventListener("click", function() {
-  const ftCaret = this.children[1].children[0].children[0];
-  const naviContent = this.nextElementSibling;
+for (let i = 0; i < collasible.length; i++) {
+  collasible[i].addEventListener("click", function() {
+    const ftCaret = this.children[1].children[0].children[0];
+    const naviContent = this.nextElementSibling;
 
-  if (!naviContent.classList.contains("visible")) {
-    this.classList.add("active");
-    naviContent.classList.add("visible");
-    ftCaret.classList.remove("fa-caret-down");
-    ftCaret.classList.add("fa-caret-up");
-  } else {
-    this.classList.remove("active");
-    naviContent.classList.remove("visible");
-    ftCaret.classList.remove("fa-caret-up");
-    ftCaret.classList.add("fa-caret-down");
-  }
-});
+    if (!naviContent.classList.contains("visible")) {
+      this.classList.add("active");
+      naviContent.classList.add("visible");
+      ftCaret.classList.remove("fa-caret-down");
+      ftCaret.classList.add("fa-caret-up");
+    } else {
+      this.classList.remove("active");
+      naviContent.classList.remove("visible");
+      ftCaret.classList.remove("fa-caret-up");
+      ftCaret.classList.add("fa-caret-down");
+    }
+  });
+}
